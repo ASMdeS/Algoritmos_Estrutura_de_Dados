d3a357892539886d25955786661626dda9cb3403
ASMdeS <arthur.souza2005@hotmail.com>
2023-12-14 21:40:31 -0300

    14/12 noite

--- Lista 3 (Tabela Hash)/B. Equipes do Cin.py
+++ Lista 3 (Tabela Hash)/B. Equipes do Cin.py
@@ -1,84 +1,55 @@
+# Criando a classe Node e definindo a chave, valor e o próximo node
 class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
+# Criando a classe Hash Table com o tamanho e a tabela em si, alpem de definir o método de inserir e imprimir a tabela
 class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [None] * size
 
-    def hash_function(self, key):
-        return hash(key) % self.size
-
     def insert(self, key, value):
-        index = self.hash_function(key)
-        if self.table[index] is None:
-            # No collision, create a new node
-            self.table[index] = Node(key, value)
+        index = value
+        node_novo = Node(key, value)
+
+        if self.table[index] is None or key < self.table[index].key:
+            node_novo.next = self.table[index]
+            self.table[index] = node_novo
         else:
-            # Collision occurred, add to the linked list
             current = self.table[index]
-            while current.next is not None:
+
+            while current.next is not None and key > current.next.key:
                 current = current.next
-            current.next = Node(key, value)
 
-    def search(self, key):
-        index = self.hash_function(key)
-        current = self.table[index]
-        while current is not None:
-            if current.key == key:
-                return current.value
-            current = current.next
-        return None
+            node_novo.next = current.next
+            current.next = node_novo
 
-    def delete(self, key):
-        index = self.hash_function(key)
-        current = self.table[index]
-        previous = None
-        while current is not None:
-            if current.key == key:
-                if previous is None:
-                    # If the node to be deleted is the first in the list
-                    self.table[index] = current.next
-                else:
-                    previous.next = current.next
-                return
-            previous = current
-            current = current.next
-
-    def imprimir_tabela(self):
+    def print_table(self):
         lista_impressao = []
-        for i, bucket in enumerate(self.table):
-            if bucket is not None:
-                for key, value in bucket:
-                    lista_impressao.append(key)
-            else:
-                lista_impressao.append('vago')
+        for index, node in enumerate(self.table):
+            lista_index = []
+            while node:
+                lista_index.append(node.key)
+                node = node.next
+                if len(lista_index) % index == 0:
+                    lista_impressao.append(lista_index)
+                    lista_index = []
         print(lista_impressao)
 
-# Example usage:
+
+# Criando a Hash Table com tamanho 10
 hash_table = HashTable(10)
 
-hash_table.insert("apple", 5)
-hash_table.insert("banana", 8)
-hash_table.insert("orange", 3)
-
-print(hash_table.search("apple"))  # Output: 5
-print(hash_table.search("banana"))  # Output: 8
-print(hash_table.search("grape"))   # Output: None
-
-hash_table.delete("banana")
-
-print(hash_table.search("banana"))  # Output: None
-
-# Recebendo entrada, dividindo ela em ordem e valor, e adicionando ou buscando e criando nova árvore
+# Recebendo o input e inserindo as informações na Hash Table
 try:
     while True:
         entrada_usuario = input().split()
-        hash_table.insert(entrada_usuario[0], entrada_usuario[1])
+        hash_table.insert(entrada_usuario[0], int(entrada_usuario[1]))
 
-# Except para a entrada vazia
+# Quando a input for vazia, a tabela hash é impressa e o código termina
 except:
-    hash_table.imprimir_tabela()
+    hash_table.print_table()
